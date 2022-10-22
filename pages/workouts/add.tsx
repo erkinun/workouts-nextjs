01be21e18135559aa0dc1b1989a7e09dd835916a
erkinun <erkinun@gmail.com>
2022-10-22 17:28:46 +0100

    refactor: move workout submit to queries module

--- pages/workouts/add.tsx
+++ pages/workouts/add.tsx
@@ -1,11 +1,11 @@
-import { ref, push } from "firebase/database";
 import { useRouter } from "next/router";
-import { database } from "../../utils/firebase";
 import LoggedIn from "../../components/LoggedIn";
 import WorkoutForm from "../../components/WorkoutForm";
 import { useAuth } from "../../utils/authContext";
 import styles from "../Dashboard.module.scss";
 import useRoutines from "../../queries/routines";
+import { Workout } from "../../utils/types";
+import { submitWorkout } from "../../queries/workouts";
 
 export default function AddWorkout() {
   const { authUser, loading } = useAuth();
@@ -13,41 +13,15 @@ export default function AddWorkout() {
   const router = useRouter();
   const routineId = router.query.routineId ?? null;
 
-  // TODO do we need a type for the workout?
-  // TODO move this fn to queries/workouts?
-  const submitWorkout = async (workout) => {
-    const uid = authUser.uid;
-
-    try {
-      const workoutsRef = ref(database, `users/${uid}/workouts`);
-      await push(workoutsRef, {
-        date: workout.date,
-        note: workout.note,
-        id: workout.id,
-        exercises: workout.exercises,
-      });
-
-      if (workout.saveAsRoutine) {
-        console.log("saving as routine");
-        const routinesRef = ref(database, `users/${uid}/routines`);
-        await push(routinesRef, {
-          exercises: workout.exercises,
-          note: workout.note,
-        });
-      }
-      router.push("/dashboard");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   if (!loading && authUser) {
     return (
       <LoggedIn>
         <main className={styles.main}>
           <h2 className={styles.header}>Add Workout</h2>
           <WorkoutForm
-            onLogWorkout={submitWorkout}
+            onLogWorkout={(workout: Workout) =>
+              submitWorkout(authUser.uid, workout, router)
+            }
             routines={routines}
             selectedRoutine={routineId}
           />
--- queries/workouts.ts
+++ queries/workouts.ts
@@ -1,8 +1,36 @@
 import { push, ref, remove, update } from "firebase/database";
+import { NextRouter } from "next/router";
 import { database } from "../utils/firebase";
 import { Workout } from "../utils/types";
 
-// TODO maybe use react query? or maybe just in react context
+// TODO optional maybe use react query? or maybe just in react context
+export const submitWorkout = async (
+  uid: string,
+  workout: Workout,
+  router: NextRouter
+) => {
+  try {
+    const workoutsRef = ref(database, `users/${uid}/workouts`);
+    await push(workoutsRef, {
+      date: workout.date,
+      note: workout.note,
+      id: workout.id,
+      exercises: workout.exercises,
+    });
+
+    if (workout.saveAsRoutine) {
+      console.log("saving as routine");
+      const routinesRef = ref(database, `users/${uid}/routines`);
+      await push(routinesRef, {
+        exercises: workout.exercises,
+        note: workout.note,
+      });
+    }
+    router.push("/dashboard");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const deleteWorkout = async (uid: string, workoutId: string) => {
   try {
